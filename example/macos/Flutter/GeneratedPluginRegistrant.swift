f9a331e352c9c3ed949262f15081bad1fbc0dda6
Nilesh Tanchak <nileshtanchak.coderkube@gmail.com>
2026-06-25 16:05:22 +0530

    add multi-platform support

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import otp_forge
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  OtpForgePlugin.register(with: registry.registrar(forPlugin: "OtpForgePlugin"))
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  otp_forge
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
